c1f1de00a7529a4f0d72a2a6a8119200731fb554
LCSSS <102164270+Lcs154@users.noreply.github.com>
2023-11-21 15:00:58 -0300

    database.sql
    Query usada na modelagem do banco de dados

---
+++ database.sql
@@ -0,0 +1,20 @@
+
+-- o banco de dados usado foi o mysql, por meio do xampp
+-- a Query usada pra modelar o banco de dados foi:
+
+CREATE DATABASE elder_admin;
+
+USE elder_admin;
+
+CREATE TABLE elder(
+	id INT AUTO_INCREMENT,
+  name VARCHAR(50) NOT NULL,
+  birthday VARCHAR(20) NOT NULL,
+  age INT NOT NULL,
+  sala VARCHAR(50),
+  email VARCHAR(70) NOT NULL,
+  numRes VARCHAR(20) NOT NULL,
+	doencas VARCHAR(150),
+    
+    CONSTRAINT elder_pk PRIMARY KEY(id)
+);
